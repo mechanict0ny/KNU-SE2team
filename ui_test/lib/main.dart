c277e17680d54827428fb8883476f9af95cf5743
Kwon-jinsol <ginsoly.knu@gmail.com>
2020-07-02 11:54:49 +0900

    최종
    앱 아이콘 및 이름 변경, 파일 이름 수정, 기타 수정

--- ui_test/lib/main.dart
+++ ui_test/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ui_test/screen/login.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'screen/home.dart';
-import 'screen/fbtest.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,22 +35,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: new ThemeData(
-//         primarySwatch: Colors.red,  //2020/06/13
-//         canvasColor: const Color(0xFFe70b0b)//배경 빨간색,
-//       ),
-//       home: MyHomePage(title: '키워드별 공지'),
-//       home: KeyApp(), 2020/06/13
-//       home: Scaffold(
-//         body: Login(),
-//       ),
-//     );
-//   }
-// }
-
-
--- ui_test/lib/screen/login.dart
+++ ui_test/lib/screen/login.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:ui_test/screen/fbtest.dart';
+import 'package:ui_test/screen/key_screen.dart';
 
 class Login extends StatefulWidget {
   @override
